2637c66489e4c58d42989ff2d1dfb2be352eb45d
DESKTOP-28L3Q6V\82103 <ssin011123@gamil.com>
2023-09-13 21:11:23 +0900

    Test : ObjectPool Dictionary Test

--- GunfireReborn_KGL_Project/Assets/Creator/SSC/Resources_SSC/Scripts/DictionarytTest.cs
+++ GunfireReborn_KGL_Project/Assets/Creator/SSC/Resources_SSC/Scripts/DictionarytTest.cs
@@ -1,39 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
 
+[Serializable]
+public class Test
+{
+    public int testAmount = 0;
+    public GameObject prefab;
+}
+
+// 인스펙터창에서 클래스 내부값 조절 접근을 위한 [Serializable]
 public class DictionarytTest : MonoBehaviour
 {
+    // 상단에 선언한 Test 클래스를 인스펙터창에서 접근하기위한 리스트 
+    [SerializeField]
+    List<Test> test;
 
-    public enum State { First, Second }
-    public State state { get; set; }
+    public GameObject[] prefabs;
 
-    Stack<int> testStack = new Stack<int>();
-    Stack<float> testStack2 = new Stack<float>();
-
+    Stack<GameObject> stack = new Stack<GameObject>();
+    Stack<GameObject> floatStack = new Stack<GameObject>();
+    Stack<GameObject> stringStack = new Stack<GameObject>();
+    
     // Start is called before the first frame update
     void Start()
     {
-        Dictionary<string, Stack<int>> bullet = new Dictionary<string, Stack<int>>();
+        Dictionary<string, Stack<GameObject>> dic = new Dictionary<string, Stack<GameObject>>();
 
-        bullet.Add("testStack", testStack);
+        for(int i  = 0; i < 10; i++)
+        {
+            stack.Push(prefabs[0]);
+            floatStack.Push(prefabs[1]);
+            stringStack.Push(prefabs[2]);
+        }   
+
+        dic.Add("Cube", stack);
+        dic.Add("Sphere", floatStack);
+        dic.Add("Capsule", stringStack);
+
+        Debug.Log("딕셔너리의 카운트값 :" + dic["Capsule"].Count);
+
+        Debug.Log("===== 인스턴스화 전 =====");
 
         for(int i = 0; i < 10; i++)
         {
-            testStack.Push(i);
+            Instantiate(prefabs[2], transform.position, transform.rotation);
+            Debug.Log("딕셔너리의" + i + " 번째 카운트 값 : " + dic["Capsule"].Count);
+            dic["Capsule"].Pop();
         }
 
-        
-        foreach(string testkey in bullet.Keys)
-        {
-            Debug.Log("현재 딕셔너리에 담긴 키 값 : " + testkey);
-        }
 
-        foreach(Stack<int> stack in bullet.Values)
-        {
-            Debug.Log("딕셔너리에 담긴 밸류값 : " + stack);
-        }
     }
 
     // Update is called once per frame
